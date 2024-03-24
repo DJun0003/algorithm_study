c42d692e76b4fe14efdbfded891f4dfab85f0c81
DJun0003 <104122340+DJun0003@users.noreply.github.com>
2024-03-24 15:36:16 +0900

    [level 2] Title: 프로세스, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -2,20 +2,16 @@ from collections import deque
 import heapq
 
 def solution(priors, location):
-    sorted_prior = sorted(priors, reverse=True)
-    sorted_prior = deque(sorted_prior)
-    pro_loc = deque([(priors[i], i) for i in range(len(priors))])
+    queue = deque([(priors[i], i) for i in range(len(priors))])
     count = 0
     
-    while len(pro_loc) > 0:
-        p, l = pro_loc.popleft()
+    while True:
+        p, l = queue.popleft()
         
-        if p < sorted_prior[0]:
-            pro_loc.append((p,l))
+        if any (p < q[0] for q in queue):
+            queue.append((p,l))
         else:
-            sorted_prior.popleft()
             count += 1
-        
             if l == location:
                 break
             
