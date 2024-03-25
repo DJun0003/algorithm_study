b5f31757024519fdc7465cb582f4c79c3d12410e
DJun0003 <104122340+DJun0003@users.noreply.github.com>
2024-03-25 11:20:40 +0900

    [level 1] Title: 폰켓몬, Time: 0.21 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,14 +1,2 @@
 def solution(nums):
-    answer = 0
-    dic = {}
-    
-    for n in nums:
-        if n in dic.keys():
-            dic[n] += 1
-        else:
-            dic[n] = 1
-    
-    if len(dic) > len(nums)/2:
-        return len(nums)/2
-    else:
-        return len(dic)
+    return min(len(nums)/2, len(set(nums)))
