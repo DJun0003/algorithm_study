88a62cf73149c48e7d6de4054983f50f506c8375
DJun0003 <104122340+DJun0003@users.noreply.github.com>
2024-03-29 11:37:22 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,27 +1,21 @@
 import time
 def solution(number, k):
-    start = time.time()
-    max_idx, max_num = 0, number[0]
-    for i in range(1, len(number)):
-        if max_num < number[i]:
-            max_idx, max_num = i, number[i]
-    if max_idx <= k:
-        number = number[max_idx:]
-        k -= max_idx
-            
-            
-    while k>0:
-        for i in range(len(number)):
-            if i == len(number) - 1:
-                number = number[:-1]
-                k -= 1
-                break
-            else:
-                if number[i] < number[i+1]:
-                    number = number[:i] + number[i+1:]
-                    k -= 1
-                    break
-    end = time.time()
-    print('%.2fms'%((end-start)*1e6))
-    return number
+    answer = [number[0]]
+    
+    for i, n in enumerate(number[1:]):
+        while answer and k and answer[-1] < n:
+            answer.pop()
+            k -= 1
+        
+        answer.append(n)
+        
+        # if k == 0:
+        #     if i < len(number)-1:
+        #         answer = answer + number[i+1:]
+        #     break
+    
+    if k > 0:
+        answer = answer[:-k]
+    
+    return ''.join(answer)
 
