117623b698c1f238bbe5e2da22a420d48c2f35b6
DJun0003 <104122340+DJun0003@users.noreply.github.com>
2024-03-30 19:49:26 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,54 +1,34 @@
 from collections import deque
 
-# def solution(begin, target, words):
-#     if target not in words:
-#         return 0
-    
-#     nodes = deque([[begin, 0]])
-#     v = [False] * len(words)
-#     while nodes:
-#         n, depth = nodes.popleft()
-#         if n == target:
-#             return depth
-        
-#         for i, w in enumerate(words):
-#             if not v[i]:
-#                 cnt = 0
-#                 for ii in range(len(n)):
-#                     if n[ii] != w[ii]:
-#                         cnt += 1
-#                 if cnt == 1:
-#                     nodes.append([w, depth+1])
-#                     v[i] = True
-#     return 0
-
 def solution(begin, target, words):
+    if target not in words:
+        return 0
     
-    if target not in words : 
-        return  0
-    
-    return bfs(begin, target, words)
-
-
-#최소 단계를 찾아야 하므로 bfs
-def bfs(begin, target, words):
-
-    queue = deque()
-    queue.append([begin, 0]) #시작 단어와 단계 0으로 초기화
-    
-    while queue:
-        now, step = queue.popleft()
+    nodes = deque([[begin, 0]])
+    v = [False] * len(words)
+    while nodes:
+        n, depth = nodes.popleft()
+        if n == target:
+            return depth
         
-        if now == target:
-            return step
-        
-        #단어를 모두 체크하면서, 해당 단어가 변경될 수 있는지 체크
-        for word in words:
-            count = 0
-            for i in range(len(now)): #단어의 길이만큼 반복하여
-                if now[i] != word[i]: #단어에 알파벳 한개씩 체크하기
-                    count += 1
-                    
-            if count == 1: 
-                queue.append([word, step+1])
+        for i, w in enumerate(words):
+            if not v[i]:
+                # cnt = 0
+                # for ii in range(len(n)):
+                #     if n[ii] != w[ii]:
+                #         cnt += 1
+                # if cnt == 1:
+                #     nodes.append([w, depth+1])
+                #     v[i] = True
+                #     # words.remove(w)
+                
+                for ii in range(len(n)-1):
+                    if n[:ii]+n[ii+1:] == w[:ii]+w[ii+1:]:
+                        nodes.append([w, depth+1])
+                        v[i] = True
+                if n[:-1] == w[:-1]:
+                    nodes.append([w, depth+1])
+                    v[i] = True
+    return 0
+
 # solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"])
