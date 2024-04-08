2c3e62648e44cdbc7553f30436dc318bacd7b2a7
DJun0003 <104122340+DJun0003@users.noreply.github.com>
2024-04-08 21:19:44 +0900

    [level 3] Title: 아이템 줍기, Time: 0.17 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -1,44 +1,19 @@
-from collections import deque
 def solution(rectangle, characterX, characterY, itemX, itemY):
     answer = 0
-    map_ = [[-1]*102 for _ in range(102)]
+    l = []
     
     # 경로 생성
-    for r in rectangle:
-        x1, y1, x2, y2 = map(lambda l: l*2, r)
-        for x in range(x1, x2+1):
-            for y in range(y1, y2+1):
-                if x==x1 or x==x2 or y==y1 or y==y2:
-                    if map_[x][y] == -1:
-                        map_[x][y] = 1
-                else:
-                    map_[x][y] = 0
-                
-    cx, cy = characterX*2, characterY*2
-    ix, iy = itemX*2, itemY*2
-    
-    queue = deque([[[cx, cy]]])
-    map_[cx][cy] = -1
-    while queue:
-        cur = queue.popleft()
-        q_list = []
-        for x, y in cur:
-            if x==ix and y==iy:
-                return answer // 2 
-            if map_[x+1][y] == 1:
-                q_list.append([x+1,y])
-                map_[x+1][y] = -1
-            if map_[x-1][y] == 1:
-                q_list.append([x-1,y])
-                map_[x-1][y] = -1
-            if map_[x][y+1] == 1:
-                q_list.append([x,y+1])
-                map_[x][y+1] = -1
-            if map_[x][y-1] == 1:
-                q_list.append([x,y-1])
-                map_[x][y-1] = -1
-        queue.append(q_list)
-        answer +=1
-                    
+    for x1,y1,x2,y2 in rectangle:
+        while len(l) < x1+1:
+            l.append([100, 0])
+        
+        ymin, ymax = l[x1]
+        if y1 < ymin:
+            l[x1][0] = y1
+        if y2 > ymax:
+            l[x1][1] = y2
+        
+    print(l)
+            
     
     return answer
