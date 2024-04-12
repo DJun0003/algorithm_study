1bc4539d0314656242f6b5e8129e2f6391a074cf
DJun0003 <104122340+DJun0003@users.noreply.github.com>
2024-04-13 01:17:44 +0900

    [level 3] Title: 네트워크, Time: 0.29 ms, Memory: 9.96 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,20 +1,63 @@
 def solution(n, computers):
     answer = 0
-    visited = [False]*n
-    
+    visited = [False] * n
     for i in range(n):
         if not visited[i]:
-            DFS(computers, visited, i)
-            # print(i, visited)
+            visited[i] = True
+            dfs(i, computers, visited)
             answer += 1
+
     return answer
 
-
-def DFS(computers, visited, node):
-    visited[node] = True
+def dfs(cur, computers, visited):
+    for i in range(len(computers)):
+        if i != cur and computers[cur][i] == 1 and visited[i] == False:
+            visited[i] = True
+            dfs(i, computers, visited)
     
-    edges = computers[node]
-    for i in range(len(edges)):
-        if edges[i] and not visited[i]:
-            DFS(computers, visited, i)
+    
+    
+
+
+
+
+
+
+
+
+
+
+# def solution(n, computers):
+#     answer = 0
+#     visited = [False]*n
+    
+#     for i in range(n):
+#         if not visited[i]:
+#             DFS(computers, visited, i)
+#             # print(i, visited)
+#             answer += 1
+#     return answer
+
+
+# def DFS(computers, visited, node):
+#     visited[node] = True
+    
+#     edges = computers[node]
+#     for i in range(len(edges)):
+#         if edges[i] and not visited[i]:
+#             DFS(computers, visited, i)
+
+            
+# def DFS(computers, visited, start):
+#     stack = [start]
+    
+#     while stack:
+#         node = stack.pop()
+#         visited[node] = True
+        
+#         edges = computers[node]
+#         for i in range(len(edges)):
+#             if edges[i] and not visited[i]:
+#                 stack.append(i)
+
     
