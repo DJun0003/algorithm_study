c21a6165cc1f81f18af6ea89bc4d472fab75f7c7
DJun0003 <104122340+DJun0003@users.noreply.github.com>
2024-04-13 13:15:11 +0900

    [level 2] Title: N-Queen, Time: 8019.87 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.py
+++ 프로그래머스/2/12952. N－Queen/N－Queen.py
@@ -1,22 +1,66 @@
 def solution(n):
     q_list = []
-    
+
     def backtracking(answer):
-        if len(q_list) == n:
-            return answer+1
+        y = len(q_list)
+        if y == n:
+            return answer + 1
         
-        for i in range(n):
-            cur = True
-            for ii, q in enumerate(q_list):
-                if i==q or i==q-len(q_list)+ii or i==q+len(q_list)-ii:
-                    cur=False
+        for x in range(n):
+            do_append = True
+            for qy, qx in enumerate(q_list):
+                if x == qx or x == qx+y-qy or x == qx-y+qy:
+                    do_append = False
                     break
-            
-            if cur:
-                q_list.append(i)
+            if do_append:
+                q_list.append(x)
                 answer = backtracking(answer)
                 q_list.pop()
         return answer
-        
+    
     return backtracking(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(n):
+#     q_list = []
+    
+#     def backtracking(answer):
+#         if len(q_list) == n:
+#             return answer+1
+        
+#         for i in range(n):
+#             cur = True
+#             for ii, q in enumerate(q_list):
+#                 if i==q or i==q-len(q_list)+ii or i==q+len(q_list)-ii:
+#                     cur=False
+#                     break
+            
+#             if cur:
+#                 q_list.append(i)
+#                 answer = backtracking(answer)
+#                 q_list.pop()
+#         return answer
+        
+#     return backtracking(0)
     
