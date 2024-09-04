2f16516be91bac9f889c95c01e0aac579fb4454d
DJun0003 <104122340+DJun0003@users.noreply.github.com>
2024-09-04 18:29:01 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 115.45 ms, Memory: 27.9 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,9 +1,7 @@
 def solution(arr):
     answer = []
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    answer.append(arr[0])
-    for v in arr[1:]:
-        if v != answer[-1]:
-            answer.append(v)
+    for num in arr:
+        if len(answer) == 0 or answer[-1] != num:
+            answer.append(num)
     
     return answer
