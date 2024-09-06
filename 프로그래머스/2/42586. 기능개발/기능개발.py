413a9962b0a9c86c4af639d35a80628f61c8d0fb
DJun0003 <104122340+DJun0003@users.noreply.github.com>
2024-09-06 09:56:43 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,34 +1,16 @@
-import math
-from collections import deque
+from math import ceil
 
 def solution(progresses, speeds):
     answer = []
-    pro = deque(progresses)
-    sp = deque(speeds)
-    
-    t_bf, num = math.ceil((100 - pro.popleft())/sp.popleft()), 1
-    while len(pro) > 0:
-        t = math.ceil((100 - pro.popleft())/sp.popleft())
-        if t > t_bf:
-            t_bf = t
+    num, t = 1, ceil((100-progresses[0]) / speeds[0])
+    for i in range(1, len(progresses)):
+        time = ceil((100-progresses[i]) / speeds[i])
+        if t < time:
             answer.append(num)
-            num = 1
+            num, t = 1, time
         else:
             num += 1
+    if num > 0:
+        answer.append(num)
         
-    
-    answer.append(num)
-    
     return answer
-
-# def solution(progresses, speeds):
-#     Q = []
-    
-#     for p, s in zip(progresses, speeds):
-#         if len(Q)==0 or Q[-1][0]<math.ceil((100-p)/s):
-#             Q.append([math.ceil((100-p)/s), 1])
-#         else:
-#             Q[-1][1] += 1
-        
-        
-#     return [q[1] for q in Q]
