efe26c73ecfca5033f8f632c7da98ad2ba5da4b7
DJun0003 <104122340+DJun0003@users.noreply.github.com>
2024-09-08 20:26:21 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.68 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,53 +1,26 @@
-from collections import deque
-from heapq import heappush, heappop
+import heapq
 
 def solution(jobs):
-    answer, len_jobs = 0, len(jobs)
-    jobs.sort()
+    jobs.sort(key = lambda x: (x[0], x[1]), reverse=True)
+    waits = []
+    cur = 0
+    ans = 0
+    num = len(jobs)
     
-    t = 0
-    wait = []
-    jobs = deque(jobs)
+    while len(waits) or len(jobs):
+        while len(jobs) and jobs[-1][0] <= cur:
+            j = jobs.pop()
+            heapq.heappush(waits, [j[1], j[0]])
+        
+        if not len(waits):
+            j = jobs.pop()
+            heapq.heappush(waits, [j[1], j[0]])
+            cur = j[0]
+        
+        w = heapq.heappop(waits)
+        cur += w[0]
+        ans += cur - w[1]
     
-    while jobs or wait:
-        # 대기열에 넣기    
-        while jobs and jobs[0][0] <= t:
-            st, cost = jobs.popleft()
-            heappush(wait, [cost, st])
-        
-        if jobs and not wait:
-            st, cost = jobs.popleft()
-            heappush(wait, [cost, st])
-            t = st
-        
-        # 작업
-        cost, st = heappop(wait)
-        t += cost
-        answer += t - st
-        
-        # print(t, t-st, (st, cost))
-    return answer // len_jobs
-
-# def solution(jobs):
-#     jobs.sort()
-#     num = len(jobs)
-#     waiting = [] # (소요시간, 요청시점)
-#     count = [] # 각 작업이 몇초 걸렸는지
-#     now = 0 #현재 시각
-
-#     while len(count) != num : 
-#         while jobs and now >= jobs[0][0] : 
-#             top = jobs.pop(0)
-#             heappush(waiting, (top[1], top[0]))
-
-#         if jobs and waiting == []:
-#             top = jobs.pop(0)
-#             now = top[0]
-#             heappush(waiting, (top[1], top[0]))
-        
-#         x,y = heappop(waiting)
-#         now += x 
-#         count.append(now-y)
-
-#     return sum(count)//num
-
+    return ans // num
+    
+    
