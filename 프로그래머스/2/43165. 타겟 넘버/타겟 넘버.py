1c79418e18da0867d0d440f285122cadcf4090f9
DJun0003 <104122340+DJun0003@users.noreply.github.com>
2024-09-10 18:15:31 +0900

    [level 2] Title: 타겟 넘버, Time: 206.21 ms, Memory: 34.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,70 +1,15 @@
 from collections import deque
 
-
 def solution(numbers, target):
     
-    adds = deque([0])
+    q = deque([0])
     
     for num in numbers:
-        l = len(adds)
-        for i in range(l):
-            a = adds.popleft()
-            adds.append(a+num)
-            adds.append(a-num)
+        new_q = deque([])
+        while q:
+            cur = q.popleft()
+            new_q.append(cur+num)
+            new_q.append(cur-num)
+        q = new_q
     
-    
-    return adds.count(target)
-    
-
-
-
-
-
-
-
-
-
-# # BFS -> 303ms, 50.1MB
-# def solution(numbers, target):
-#     answer = 0
-#     leaves = [0]
-    
-#     for num in numbers:
-#         tmp = []
-#         for parent in leaves:
-#             tmp.append(parent + num)
-#             tmp.append(parent - num)
-#         leaves = tmp
-    
-#     answer = sum(map(lambda x: x == target, leaves))
-    
-#     return answer
-
-
-# # DFS -> 834ms
-# def solution(numbers, target):
-#     answer = DFS(numbers, target, 0)
-#     return answer
-
-# def DFS(numbers, target, depth):
-#     answer = 0
-#     if depth == len(numbers):
-#         if sum(numbers) == target:
-#             return 1
-#         else:
-#             return 0
-    
-#     else:
-#         answer += DFS(numbers, target, depth+1)
-#         numbers[depth] *= -1
-#         answer += DFS(numbers, target, depth+1)
-#         return answer
-
-# # 짧은코드 -> 355ms, 34.1MB
-# from itertools import product
-# def solution(numbers, target):
-#     l = [(x, -x) for x in numbers]
-#     a = list(map(sum, product(*l)))
-#     return a.count(target)
-
-            
+    return q.count(target)
