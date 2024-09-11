cde86c04ee3a8245f3c8dd2f374ea024d6cf67fc
DJun0003 <104122340+DJun0003@users.noreply.github.com>
2024-09-11 20:16:24 +0900

    [level 1] Title: 모의고사, Time: 5.80 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,16 +1,20 @@
 def solution(answers):
-    s1list = [5,1,2,3,4]
-    s2list = [5,2,1,2,3,2,4,2]
-    s3list = [5,3,3,1,1,2,2,4,4,5]
-    score = [0,0,0]
+    a1 = [1,2,3,4,5]
+    a2 = [2,1,2,3,2,4,2,5]
+    a3 = [3,3,1,1,2,2,4,4,5,5]
+    score = [0, 0, 0]
     
     for i, ans in enumerate(answers):
-        score[0] += int(s1list[(i+1)%5] == ans)
-        score[1] += int(s2list[(i+1)%8] == ans)
-        score[2] += int(s3list[(i+1)%10] == ans)
-        
-    maxs, answer = max(score), []
-    for i in range(3):
-        if score[i] == maxs:
+        if a1[i%len(a1)] == ans:
+            score[0] += 1
+        if a2[i%len(a2)] == ans:
+            score[1] += 1
+        if a3[i%len(a3)] == ans:
+            score[2] += 1
+    
+    max_score = max(score)
+    answer = []
+    for i in range(len(score)):
+        if score[i] == max_score:
             answer.append(i+1)
     return answer
