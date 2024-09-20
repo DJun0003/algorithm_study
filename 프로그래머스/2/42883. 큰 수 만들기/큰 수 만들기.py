6bf3122de0456d9795f1a2f6d11e12ff3cc033ce
DJun0003 <104122340+DJun0003@users.noreply.github.com>
2024-09-20 16:53:03 +0900

    [level 2] Title: 큰 수 만들기, Time: 119.81 ms, Memory: 13.6 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,21 +1,14 @@
-import time
 def solution(number, k):
     answer = [number[0]]
-    
-    for i, n in enumerate(number[1:]):
-        while answer and k and answer[-1] < n:
+    for i, num in enumerate(number[1:]):
+        while answer and k and answer[-1] < num:
             answer.pop()
             k -= 1
         
-        answer.append(n)
-        
-        # if k == 0:
-        #     if i < len(number)-1:
-        #         answer = answer + number[i+1:]
-        #     break
+        answer.append(num)
     
-    if k > 0:
+    if k:
         answer = answer[:-k]
     
     return ''.join(answer)
-
+        
