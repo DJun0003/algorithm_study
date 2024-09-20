209124fbec414d7fe9ac6e6876dbb25b946907a0
DJun0003 <104122340+DJun0003@users.noreply.github.com>
2024-09-20 17:15:21 +0900

    [level 2] Title: 소수 찾기, Time: 9.42 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,31 +1,24 @@
 from itertools import permutations
 from math import sqrt
 
-def is_prime(num):
-    if num == 0 or num == 1:
-        return False
-    sq = sqrt(num)
-    is_ = True
-    if type(sq) != int:
-        for i in range(2, int(sq)+1):
-            if num % i == 0:
-                is_ = False
-    return is_
-        
-
 def solution(numbers):
-    ans_list = []
-    for i in range(1,len(numbers)+1):
-        for n in list(permutations(numbers, i)):
-            num = int(''.join(n))
-            if num in ans_list:
-                continue
-            elif is_prime(num):
-                ans_list.append(num)
-    return len(ans_list)
+    num_list = []
+    for n in range(1, len(numbers)+1):
+        num_list += list(map(lambda x: int(''.join(x)), permutations(numbers, n)))
+    
+    ans = []
+    for num in set(num_list):
+        if num==0 or num==1:
+            continue
+
+        is_p = True
+        for i in range(2, int(sqrt(num))+1):
+            if num%i == 0:
+                is_p = False
+                break
+        if is_p:
+            ans.append(num)
+    
+    return len(ans)
             
             
-        
-        
-    
-    
