6f8c5924ff3e7e13b8df7403ecd5b9929cf59a65
DJun0003 <104122340+DJun0003@users.noreply.github.com>
2024-09-23 15:04:00 +0900

    [level 3] Title: 아이템 줍기, Time: 1.72 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -1,19 +1,33 @@
-def solution(rectangle, characterX, characterY, itemX, itemY):
-    answer = 0
-    l = []
+from collections import deque
+
+def solution(rectangle, chx, chy, itx, ity):
+    maps = [[-1] * 102 for _ in range(102)]
+    for r in rectangle:
+        x1, y1, x2, y2 = map(lambda x: x*2, r)
+        for y in range(y1, y2+1):
+            for x in range(x1, x2+1):
+                if y==y1 or y==y2 or x==x1 or x==x2:
+                    if maps[y][x] == -1:
+                        maps[y][x] = 1
+                else:
+                    maps[y][x] = 0
     
-    # 경로 생성
-    for x1,y1,x2,y2 in rectangle:
-        while len(l) < x1+1:
-            l.append([100, 0])
+    chy, chx = chy*2, chx*2
+    ity, itx = ity*2, itx*2
+    q = deque([[chy, chx, 0]])
+    maps[chy][chx] = -1
+    
+    while q:
+        cury, curx, ans = q.popleft()
+        if cury==ity and curx==itx:
+            return ans
         
-        ymin, ymax = l[x1]
-        if y1 < ymin:
-            l[x1][0] = y1
-        if y2 > ymax:
-            l[x1][1] = y2
-        
-    print(l)
+        for my, mx in [[1,0], [-1,0], [0,1], [0,-1]]:
+            ny, newy, nx, newx = cury+my, cury+(2*my), curx+mx, curx+(2*mx)
+            if newy<2 or newy>100 or newx<2 or newx>100:
+                continue
+            if maps[newy][newx]==1 and maps[ny][nx]==1:
+                q.append([newy, newx, ans+1])
+                maps[newy][newx] = -1
             
-    
-    return answer
+                    
