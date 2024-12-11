8ae17565b3bab4a4b60df62a4891d4efbf325a5d
DJun0003 <104122340+DJun0003@users.noreply.github.com>
2024-12-11 14:03:01 +0900

    [level 2] Title: 타겟 넘버, Time: 489.84 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,14 +1,23 @@
 from collections import deque
 
 def solution(numbers, target):
-    cur_list = [0]
-    numbers = deque(numbers)
+    answer = 0
+    leaves = [0]
     
+    adds = deque([0])
+
     for num in numbers:
-        new_list = []
-        for cur in cur_list:
-            new_list.append(cur+num)
-            new_list.append(cur-num)
-        cur_list = new_list
+        tmp = []
+        for parent in leaves:
+            tmp.append(parent + num)
+            tmp.append(parent - num)
+        leaves = tmp
+        l = len(adds)
+        for i in range(l):
+            a = adds.popleft()
+            adds.append(a+num)
+            adds.append(a-num)
     
-    return cur_list.count(target)
+
+    answer = sum(map(lambda x: x == target, leaves))
+    return adds.count(target)
