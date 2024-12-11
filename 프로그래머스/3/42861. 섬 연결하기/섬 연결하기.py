714ffefda2aa46860170f8a768feb9ef2ef3daef
DJun0003 <104122340+DJun0003@users.noreply.github.com>
2024-12-11 22:11:57 +0900

    [level 3] Title: 섬 연결하기, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,26 +1,25 @@
 def find(x, parents):
-    if x!= parents[x]:
+    if x != parents[x]:
         parents[x] = find(parents[x], parents)
-    
+        
     return parents[x]
 
-def union(st, de, parents):
-    st = find(st, parents)
-    de = find(de, parents)
+def union(x, y, parents):
+    x = find(x, parents)
+    y = find(y, parents)
     
-    parents[de] = st
-    
-
+    parents[y] = x
 
 def solution(n, costs):
-    costs.sort(key=lambda x: x[-1])
-    parents = [i for i in range(n)]
     answer = 0
+    costs.sort(key=lambda x: x[-1])
     
-    for [st, de, c] in costs:
-        if find(st, parents) != find(de, parents):
+    parents = [i for i in range(n)]
+    
+    for [x,y,c] in costs:
+        if find(x, parents) != find(y, parents):
+            union(x, y, parents)
             answer += c
-            union(st, de, parents)
+    
     
     return answer
-    
