860bf4c5c8911ef6ab8f2594c2015fd487cd5487
DJun0003 <104122340+DJun0003@users.noreply.github.com>
2024-12-11 22:23:31 +0900

    [level 2] Title: 배달, Time: 1.96 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -2,26 +2,33 @@ import heapq
 
 def solution(N, road, K):
     answer = 0
-    graph = [[] for _ in range(N+1)]
     
-    for [n1, n2, c] in road:
-        graph[n1].append([n2, c])
-        graph[n2].append([n1, c])
+    graph = [{} for _ in range(N+1)]
     
+    for x, y, c in road:
+        if y in graph[x]:
+            graph[x][y] = min(c, graph[x][y])
+            graph[y][x] = min(c, graph[y][x])
+        else:
+            graph[x][y] = c
+            graph[y][x] = c
+    
+    l = [1e9] * (N+1)
+    l[1] = 0
     q = [[0, 1]]
     heapq.heapify(q)
-    length = [1e9] * (N+1)
-    length[1] = 0
     
     while q:
         cost, cur = heapq.heappop(q)
         
-        if length[cur] < cost:
+        if l[cur] < cost:
             continue
         
-        for [des, c] in graph[cur]:
-            if cost+c < length[des]:
-                length[des] = cost+c
-                heapq.heappush(q, [cost+c, des])
-        
-    return sum(map(lambda x: x<=K, length))
+        for des, c in graph[cur].items():
+            new_cost = cost+c
+            
+            if new_cost < l[des]:
+                l[des] = new_cost
+                heapq.heappush(q, [new_cost, des])
+    
+    return sum(map(lambda x: x<=K, l))
