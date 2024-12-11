c355349dc1f7cef269371c9b4ac573115408b931
DJun0003 <104122340+DJun0003@users.noreply.github.com>
2024-12-11 13:26:02 +0900

    [level 2] Title: 프로세스, Time: 0.35 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,17 +1,19 @@
 from collections import deque
 
 def solution(priorities, location):
-    total = len(priorities)
     priorities = deque(priorities)
-    loc = deque([i for i in range(total)])
+    total_p = len(priorities)
+    max_p = max(priorities)
+    while True:
+        cur_p = priorities.popleft()
+        if cur_p < max_p:
+            priorities.append(cur_p)
+            location = (location-1+len(priorities)) % len(priorities)
+        else:
+            if location == 0:
+                return total_p - len(priorities)
+            else:
+                location -= 1
+                max_p = max(priorities)
     
-    while loc:
-        p, l = priorities.popleft(), loc.popleft()
-        if loc and p < max(priorities):
-            priorities.append(p)
-            loc.append(l)
-        elif l == location:
-            return total - len(loc)
-    
-    return total
-            
+        
