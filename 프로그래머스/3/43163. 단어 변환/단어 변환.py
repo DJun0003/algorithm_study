7f36895526d87f73cb42695186c4ce2ea54ba71b
DJun0003 <104122340+DJun0003@users.noreply.github.com>
2024-12-12 11:31:50 +0900

    [level 3] Title: 단어 변환, Time: 0.21 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,26 +1,35 @@
 from collections import deque
 
+def can_change(a, b):
+    diff = 0
+    for i in range(len(a)):
+        if a[i]!=b[i]:
+            diff += 1
+        if diff > 1:
+            return False
+    return True
+
 def solution(begin, target, words):
-    
+    answer = 0
+
     if not target in words:
         return 0
     
     q = deque([[begin]])
     visited = [False] * len(words)
-    count = 0
     
     while q:
         cur_list = q.popleft()
-        new_list = []
-        for cur in cur_list:
-            if cur==target:
-                return count
-            
+        next_list = []
+        
+        answer += 1
+        for w in cur_list:
             for i in range(len(words)):
-                if not visited[i] and sum(map(lambda x: words[i][x]!=cur[x], range(len(cur))))==1:
-                    new_list.append(words[i])
+                if not visited[i] and can_change(w, words[i]):
+                    if words[i] == target:
+                        return answer
+                    
                     visited[i] = True
-        count += 1
-        q.append(new_list)
-    
-    return count
+                    next_list.append(words[i])
+        q.append(next_list)
+    return 0
